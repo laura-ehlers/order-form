a8d10693c32c0862a8e9b85ca611ed94a34c8a02
Laura Schumacher <lschumacher876@gmail.com>
2021-07-31 16:52:13 +0200

    added steps between product list and billing address

--- order.js
+++ order.js
@@ -1,3 +1,5 @@
+let currentPage = 'product-list';
+
 function decrement(object) {
     console.log(object.parentElement.childNodes[3].textContent);
     object.parentElement.childNodes[3].textContent--;
@@ -6,4 +8,28 @@ function decrement(object) {
 function increment(object) {
     console.log(object);
     object.parentElement.childNodes[3].textContent++;
+}
+
+function prev() {
+    if (currentPage == 'billing-address') {
+        document.getElementById('product-list').style.display = 'inline-block';
+        document.getElementById('billing-address').style.display = 'none';
+        document.getElementById('prev').classList.toggle('disabled-button');
+        document.getElementById('next').classList.toggle('disabled-button');
+        currentPage = 'product-list';
+    }
+}
+
+function next() {
+    if (currentPage == 'product-list') {
+        document.getElementById('product-list').style.display = 'none';
+        document.getElementById('billing-address').style.display = 'inline-block';
+        document.getElementById('prev').classList.toggle('disabled-button');
+        document.getElementById('next').classList.toggle('disabled-button');
+        currentPage = 'billing-address';
+    }
+}
+
+function placeOrder() {
+    
 }
